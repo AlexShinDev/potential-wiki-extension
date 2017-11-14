7f5d37e5584de3939f3da49a8f9c38149892dcc3
xthegoodguy <xashinx@gmail.com>
2017-11-14 16:29:10 -0600

    styling, routes

--- options.js
+++ options.js
@@ -1,19 +1,27 @@
+// $(function(){
+
+//     chrome.storage.sync.get('limit',function(budget){
+//         $('#limit').val(budget.limit);
+//     });
+
+//     $('#saveLimit').click(function(){
+//         var limit = $('#limit').val();
+//         if (limit){
+//             chrome.storage.sync.set({'limit': limit}, function(){
+//                 close();
+//             });
+//         }
+//     });
+
+//     $('#resetTotal').click(function(){
+//         chrome.storage.sync.set({'total': 0});
+//     });
+// });
+
 $(function(){
-
-    chrome.storage.sync.get('limit',function(budget){
-        $('#limit').val(budget.limit);
-    });
-
-    $('#saveLimit').click(function(){
-        var limit = $('#limit').val();
-        if (limit){
-            chrome.storage.sync.set({'limit': limit}, function(){
-                close();
-            });
-        }
-    });
-
-    $('#resetTotal').click(function(){
-        chrome.storage.sync.set({'total': 0});
+  $("button").click(function(){
+    $.get("http://localhost:3000/api/v1/users/1.json".body, function(data){
+        console.log(data);
+      });
     });
 });
